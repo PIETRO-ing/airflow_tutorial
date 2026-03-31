3fe2cb8f01ce13390cd985910555826e5169fd96
PIETRO-ing <pietro.passarella@proton.me>
2026-03-31 02:53:38 +0200

    first dag added

---
+++ dags/1_first_dag.py
@@ -0,0 +1,27 @@
+from airflow.sdk import dag, task
+
+@dag
+def first_dag():
+    
+    @task.python
+    def first_task():
+        print("This is the first task")
+
+    @task.python
+    def second_task():
+        print("This is the second task")
+
+    @task.python
+    def third_task():
+        print("This is the third task")
+
+    # Defining task dependencies
+    first = first_task()
+    second = second_task()
+    third = third_task()
+
+
+    first >> second >> third
+
+first_dag()
+
