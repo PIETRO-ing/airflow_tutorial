f4eca5551b67cb08c826c3b71e5b99eb4e2c4c84
PIETRO-ing <pietro.passarella@proton.me>
2026-04-02 14:59:28 +0200

    schedule dag added

---
+++ dags/8_schedule_preset.py
@@ -0,0 +1,32 @@
+from airflow.sdk import dag, task
+from pendulum import datetime
+
+@dag(
+        dag_id="first_dag",
+        start_date= datetime(year=2026, month=4, day=2, tz='CET'),
+        schedule="@daily",
+        is_paused_upon_creation=False
+)
+def first_schedule_dag():
+    
+    @task.python
+    def first_task():
+        print("This is the first task")
+
+    @task.python
+    def second_task():
+        print("This is the second task")
+
+    @task.python
+    def third_task():
+        print("This is the third task")
+
+    # Defining task dependencies
+    first = first_task()
+    second = second_task()
+    third = third_task()
+
+
+    first >> second >> third
+
+first_schedule_dag()
